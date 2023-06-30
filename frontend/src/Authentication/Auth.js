058e333e25d81bad2bec14c1672f1161f7687cfa
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2023-06-30 15:27:23 +0200

    still need to fix the css for login and register

--- frontend/src/Authentication/Auth.js
+++ frontend/src/Authentication/Auth.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import {useState,React} from 'react';
 import Name from '../Common/Name';
+import TabSwitcher from '../Common/TabSwitcher';
+import Login from '../Authentication/Login/Login';
+import Register from './Register/Register';
+
+
+
 function Auth() {
+  const tabs = ['Login', 'Sign-up',];
+  const [flag, setFlag] = useState(false);
+
+  const handleTab1Click = () => {
+    setFlag(false);
+    
+  };
+  
+  const handleTab2Click = () => {
+    setFlag(true);
+   
+  };
+  
+  
+
+  const tabFunctions = [handleTab1Click, handleTab2Click];
   return (
     <div>
       <Name/>
+      <TabSwitcher tabs={tabs} tabFunctions={tabFunctions}/>
+      
+      {flag?<Register/>:<Login/>}
     </div>
   )
 }
