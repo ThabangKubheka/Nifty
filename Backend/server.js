28aac7064a2d045fb46707f8f48efb926b30cb2c
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2023-07-28 14:14:05 +0200

    Updated port

--- Backend/server.js
+++ Backend/server.js
@@ -26,5 +26,5 @@ const { pathname } = url.parse(req.url, true);
 });
 
 server.listen(4000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 4000');
 });
